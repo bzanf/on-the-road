bfe193bf51148fcf0b559a42b5381d7bc7ab97d2
bzanf <borysfreitas@gmail.com>
2024-09-24 18:53:42 -0300

    Create vehicles-page component

---
+++ src/app/features/vehicles/pages/vehicles-page/vehicles-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicles-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicles-page.component.html',
+  styleUrl: './vehicles-page.component.scss'
+})
+export class VehiclesPageComponent {
+
+}
