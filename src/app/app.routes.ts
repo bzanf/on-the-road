f986c5288f9d4ac1b3a7a55e03beb8d1bf4603eb
bzanf <borysfreitas@gmail.com>
2024-09-26 20:08:44 -0300

    Add vehicles route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { VehiclesPageComponent } from './features/vehicles/pages/vehicles-page/vehicles-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "vehicles", component: VehiclesPageComponent }
+];
