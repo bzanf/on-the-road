8e06bba44f5a417faaf2b08d0c4ea0cc25b74e0f
bzanf <borysfreitas@gmail.com>
2024-09-30 23:09:36 -0300

    Improve readability by fixing indentation

--- src/app/domain/repositories/vehicle.repository.ts
+++ src/app/domain/repositories/vehicle.repository.ts
@@ -2,7 +2,6 @@ import { Observable } from "rxjs";
 import { Vehicle } from "../entities/vehicle.entity";
 
 export abstract class VehicleRepository {
-
     abstract getAll(): Observable<Vehicle[]>;
     abstract add(vehicle: Vehicle): Observable<Vehicle>;
 }
