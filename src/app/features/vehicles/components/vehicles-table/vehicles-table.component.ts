34c4f7502b6538280c30274a2a6046956e34f0c4
bzanf <borysfreitas@gmail.com>
2024-09-30 22:54:04 -0300

    Update vehicle table component to use states

--- src/app/features/vehicles/components/vehicles-table/vehicles-table.component.ts
+++ src/app/features/vehicles/components/vehicles-table/vehicles-table.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Vehicle } from '../../../../domain/entities/vehicle.entity';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'vehicles-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vehicles-table.component.html',
   styleUrl: './vehicles-table.component.scss'
 })
 export class VehiclesTableComponent {
 
-  @Input() data: Vehicle[] = [];
+  @Input() data$!: Observable<Vehicle[]>;
 
 }
--- src/app/features/vehicles/pages/vehicles-page/vehicles-page.component.ts
+++ src/app/features/vehicles/pages/vehicles-page/vehicles-page.component.ts
@@ -1,29 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { VehiclesTableComponent } from "../../components/vehicles-table/vehicles-table.component";
 import { Vehicle } from '../../../../domain/entities/vehicle.entity';
-import { VehicleService } from '../../services/vehicles.service';
+import { Store } from '@ngrx/store';
+import { selectAll, selectLoading } from '../../store/vehicle.selectors';
+import { load, add } from '../../store/vehicle.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'vehicles-page',
   standalone: true,
   imports: [VehiclesTableComponent],
   templateUrl: './vehicles-page.component.html',
-  styleUrl: './vehicles-page.component.scss',
+  styleUrl: './vehicles-page.component.scss'
 })
 export class VehiclesPageComponent implements OnInit {
 
-  vehicles: Vehicle[] = [];
+  vehicles$: Observable<Vehicle[]>;
+  loading$: Observable<boolean>;
 
-  constructor(
-    private vehicleService: VehicleService
-  ) { }
-
-  ngOnInit(): void {
-    this.fetchData();
+  constructor(private store: Store) {
+    this.vehicles$ = this.store.select(selectAll);
+    this.loading$ = this.store.select(selectLoading);
   }
 
-  async fetchData() {
-    this.vehicles = await this.vehicleService.getAll();
+  ngOnInit(): void {
+    this.loadVehicles();
+  }
+
+  loadVehicles() {
+    this.store.dispatch(load());
+  }
+
+  addVehicle() {
+    const vehicle = new Vehicle(1, "ZZZ1234", "ZZZZ", "YYYY", 1, 3);
+    this.store.dispatch(add({ vehicle }));
+  }
+
+  test() {
+
   }
 
 }
