8516e2d6d7749137ea420f852a5dc322c5e57b8b
bzanf <borysfreitas@gmail.com>
2024-09-30 22:55:17 -0300

    Add ngrx store and effects dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { VehicleInMemoryRepository } from './infrastructure/repositories/vehicle-in-memory.repository';
 import { VehicleRepository } from './domain/repositories/vehicle.repository';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { VehicleEffects } from './features/vehicles/store/vehicle.effects';
+import { vehicleReducer } from './features/vehicles/store/vehicle.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    { provide: VehicleRepository, useClass: VehicleInMemoryRepository }
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore({ vehicle: vehicleReducer }),
+    provideEffects(VehicleEffects),
+    { provide: VehicleRepository, useClass: VehicleInMemoryRepository },
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 import { VehiclesPageComponent } from './features/vehicles/pages/vehicles-page/vehicles-page.component';
 
 export const routes: Routes = [
-    { path: "vehicles", component: VehiclesPageComponent }
+    {
+        path: "vehicles", component: VehiclesPageComponent,
+    }
 ];
