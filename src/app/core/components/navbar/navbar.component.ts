2ba3239d8eee6fa7bf50fa5e71c13ca87b907361
bzanf <borysfreitas@gmail.com>
2024-10-01 21:18:47 -0300

    Add route/button to vehicles page and create routeIsActive method

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  constructor(private router: Router) {}
+
+  routeIsActive(path: string) {
+    return this.router.url === path;
+  }
 
 }
