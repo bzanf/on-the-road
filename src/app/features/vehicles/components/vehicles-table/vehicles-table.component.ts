4bd5c5c796acc5923fd0ef2d998f264c6e40c9b4
bzanf <borysfreitas@gmail.com>
2024-10-05 01:46:48 -0300

    Replace initial test table with "ag-grid"

--- src/app/features/vehicles/components/vehicles-table/vehicles-table.component.ts
+++ src/app/features/vehicles/components/vehicles-table/vehicles-table.component.ts
@@ -2,16 +2,27 @@ import { Component, Input } from '@angular/core';
 import { Vehicle } from '../../../../domain/entities/vehicle.entity';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+import { randomVehicles } from '../../../../shared/utils/random-vehicle';
 
 @Component({
   selector: 'vehicles-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AgGridAngular],
   templateUrl: './vehicles-table.component.html',
   styleUrl: './vehicles-table.component.scss'
 })
 export class VehiclesTableComponent {
 
   @Input() data$!: Observable<Vehicle[]>;
+
+  colDefs: ColDef<Vehicle, string>[] = [ 
+    { field: "plate" },
+    { valueGetter: params => (params.data?.brand + '/' + params.data?.model) },
+    { field: "type" },
+  ];
+
+  data: Vehicle[] = randomVehicles(5); 
 
 }
