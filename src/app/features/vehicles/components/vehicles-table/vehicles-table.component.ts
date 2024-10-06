5f90067c4ce38a869b58af67cd671c278b388330
bzanf <borysfreitas@gmail.com>
2024-10-06 17:43:06 -0300

    Add auto size columns and column header label

--- src/app/features/vehicles/components/vehicles-table/vehicles-table.component.ts
+++ src/app/features/vehicles/components/vehicles-table/vehicles-table.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { AgGridAngular } from 'ag-grid-angular';
-import { ColDef } from 'ag-grid-community';
-import { randomVehicles } from '../../../../shared/utils/random-vehicle';
+import { ColDef, FirstDataRenderedEvent } from 'ag-grid-community';
 import { Vehicle } from '../../../../core/domain/entities/vehicle.entity';
 
 @Component({
@@ -17,12 +16,17 @@ export class VehiclesTableComponent {
 
   @Input() data$!: Observable<Vehicle[]>;
 
-  colDefs: ColDef<Vehicle, string>[] = [ 
+  colDefs: ColDef<Vehicle, string>[] = [
     { field: "plate" },
-    { valueGetter: params => (params.data?.brand + '/' + params.data?.model) },
+    {
+      headerName: 'Brand/Model',
+      valueGetter: params => (params.data?.brand + '/' + params.data?.model)
+    },
     { field: "type" },
   ];
 
-  data: Vehicle[] = randomVehicles(5); 
+  onFirstDataRendered(param: FirstDataRenderedEvent) {
+    param.api.sizeColumnsToFit();
+  }
 
 }
