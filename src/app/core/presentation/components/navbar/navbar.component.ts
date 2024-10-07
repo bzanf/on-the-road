5991e763fd9754fb66afcd69d84b393533cd5869
bzanf <borysfreitas@gmail.com>
2024-10-06 21:10:08 -0300

    Add bootstrap tooltip to navbar buttons

--- src/app/core/presentation/components/navbar/navbar.component.ts
+++ src/app/core/presentation/components/navbar/navbar.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { TooltipDirective } from '../../../../shared/directives/tooltip/tooltip.directive';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, TooltipDirective],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   routeIsActive(path: string) {
     return this.router.url === path;
