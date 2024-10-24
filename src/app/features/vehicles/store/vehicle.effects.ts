588a6e85c9bfe767a07dadfed7a212ce47708096
bzanf <borysfreitas@gmail.com>
2024-10-24 19:18:56 -0300

    Remove vehicle service

--- src/app/features/vehicles/store/vehicle.effects.ts
+++ src/app/features/vehicles/store/vehicle.effects.ts
@@ -1,8 +1,8 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { VehicleService } from "../services/vehicle.service";
 import { Injectable } from "@angular/core";
 import * as VehicleActions from './vehicle.actions';
 import { catchError, map, mergeMap, of } from "rxjs";
+import { VehicleRepository } from "../../../core/domain/repositories/vehicle.repository";
 
 @Injectable()
 export class VehicleEffects {
@@ -13,13 +13,13 @@ export class VehicleEffects {
 
     constructor(
         private actions$: Actions,
-        private vehicleService: VehicleService,
+        private vehicleRepository: VehicleRepository, 
     ) {
         this.loadVehicles$ = createEffect(() =>
             this.actions$.pipe(
                 ofType(VehicleActions.load),
                 mergeMap(() =>
-                    this.vehicleService.getAll().pipe(
+                    this.vehicleRepository.getAll().pipe(
                         map(vehicles => VehicleActions.loadSuccess({ vehicles })),
                         catchError(error => of(VehicleActions.loadFailure({ error })))
                     )
@@ -31,7 +31,7 @@ export class VehicleEffects {
             this.actions$.pipe(
                 ofType(VehicleActions.add),
                 mergeMap(action =>
-                    this.vehicleService.add(action.vehicle).pipe(
+                    this.vehicleRepository.add(action.vehicle).pipe(
                         map(vehicle => VehicleActions.addSuccess({ vehicle })),
                         catchError(error => of(VehicleActions.addFailure({ error })))
                     )
@@ -43,7 +43,7 @@ export class VehicleEffects {
             this.actions$.pipe(
                 ofType(VehicleActions.delete_),
                 mergeMap(action =>
-                    this.vehicleService.delete(action.id).pipe(
+                    this.vehicleRepository.delete(action.id).pipe(
                         map(() => VehicleActions.deleteSuccess({ id: action.id })),
                         catchError(error => of(VehicleActions.deleteFailure({ error })))
                     )
